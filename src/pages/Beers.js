b41690b22a2ba9ddf21e466f0056525bbdce9614
Juliana Felix <julianafelix.dev@gmail.com>
2022-06-16 01:49:14 -0300

    api first connection

--- src/pages/Beers.js
+++ src/pages/Beers.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { api } from "../services/api";
 
 // import { Container } from './styles';
 
 export function Beers() {
-  return <button>oi</button>;
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    api.get().then((response) => {
+      setBeers(response.data);
+    });
+  }, []);
+
+  return (
+    <ul>
+      {beers.map((data) => {
+        return (
+          <li key={data.name}>
+            <p>{data.name}</p>
+            <button>oi</button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: ["./src/**/*.js"],
   theme: {
     extend: {},
   },
