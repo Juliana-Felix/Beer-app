35d014ea2c1a5d6f6572e117d3cd5143a49d3d84
Juliana Felix <julianafelix.dev@gmail.com>
2022-06-21 21:34:26 -0300

    test deploy

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
-
+const app = express();
 const { resolve } = require("path");
 
-app.use("/", express.static(resolve(__dirname, "./dist")));
-
-const app = express();
+app.use(express.static(resolve(__dirname, "./public")));
 
 app.listen(process.env.PORT || 8080, (err) => {
   if (err) {
