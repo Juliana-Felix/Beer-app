a4d3d92f4e089970d3b0793ebc4bde5ea9945bb5
Juliana Felix <julianafelix.dev@gmail.com>
2022-06-21 21:51:51 -0300

    test deployconfig

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const path = require("path");
-const port = process.env.PORT || 8080;
+
 const app = express();
-app.use(express.static(__dirname + "/public"));
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
-app.listen(port);
-console.log("Server started on port " + port);
+
+const baseDir = `${__dirname}/build/`;
+
+app.use(express.static(`${baseDir}`));
+
+app.get("*", (req, res) => res.sendFile("index.html", { root: baseDir }));
+
+const port = 4000;
+
+app.listen(port, () =>
+  console.log(`Servidor subiu com sucesso em http://localhost:${port}`)
+);
